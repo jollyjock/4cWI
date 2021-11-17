54387406083d0128927339d931f0167a94039195
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2021-11-17 10:14:37 +0100

    init

---
+++ Javascript/script.js
@@ -0,0 +1,19 @@
+// 1. Rechnung 
+
+let a = 17.4;
+let b = 3.9;
+
+let ab = a/b;
+console.log("Das Ergbnis von " + a + " und " + b + " ist " + ab);
+
+
+
+// 2. Name ausgeben 
+
+let firstname = "Stephanie";
+let lastname = "Lohitzer";
+console.log(firstname);
+console.log(lastname);
+
+console.log("Ich heiße " + firstname + lastname );
+
