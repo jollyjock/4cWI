ac840d63f5413f6f24c7ca97a60d209582364607
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2021-11-18 11:25:23 +0100

    Hello

---
+++ Javascript/Aufgabe 3/Variablen.js
@@ -0,0 +1,36 @@
+
+//Erstelle eine Variable banana mit dem Wert banana
+let banana = "Banana";
+
+//Erstelle eine Variable apple mit dem Wert apple
+let apple = "Apple";
+
+
+//Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+let bananaPricePerKilo = 2.14;
+
+//Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+let applePricePerKilo = 3.43;
+
+let bananaWeight = 0.22;
+let appleWeight = 0.34;
+
+
+//Preis von 8 Äpfeln
+let PriceOf8Apple = (appleWeight * applePricePerKilo)*8;
+console.log("Der Preis von 8 Äpfeln beträgt " + PriceOf8Apple);
+
+//Preis von 17 Bananen 
+let PriceOf17Banana = (bananaWeight * bananaPricePerKilo) * 17;
+console.log("Preis von 17 Bananen beträgt " + PriceOf17Banana);
+
+//Preis von 1 Tonne Äpfel
+let PriceOf1TonApple = (appleWeight * applePricePerKilo) *1000;
+console.log("Preis von 1 Tonne Äpfeln beträgt " + PriceOf1TonApple);
+
+//Preis von 1 Tonne Bananen
+let PriceOf1TonBanana = (bananaWeight * bananaPricePerKilo) *1000;
+console.log("Preis von 1 Tonne Banana beträgt " + PriceOf1TonBanana);
+
+
+
