9281bf885d919e15f2b05680a4c013bc328d0173
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2021-11-23 17:00:31 +0100

    lee

--- Javascript/Aufgabe 5/IF.js
+++ Javascript/Aufgabe 5/IF.js
@@ -10,6 +10,8 @@ if (number < 25){
 }
 */
 
+//Aufgabe 1
+
 let number = Math.random() * 100;
 let number15 = Math.round(number);
 console.log(number15)
@@ -27,4 +29,26 @@ if (number15 > 52){
     console.log("Large")
 }
 
+//Aufgabe 2
+
+let number420 = Math.random() * 100;
+let number69 = Math.round(number420);
+console.log(number69)
+
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+if (number69 <=7){
+    console.log("Mini")
+}
+
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+
+if (number69 > 9 && number69 < 90){
+    console.log("Medium")
+}
+
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+
+if (number69 > 92){
+    console.log("Large")
+}
 
