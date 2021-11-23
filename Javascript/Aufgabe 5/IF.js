712561b99d93ce5e5b729727f120148e8e475a7b
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2021-11-23 16:50:41 +0100

    yooooo

--- Javascript/Aufgabe 5/IF.js
+++ Javascript/Aufgabe 5/IF.js
@@ -1,6 +1,30 @@
+/*
+let number = 33; 
 
-let number = 175; 
-
-if (number <= 20){
-    console.log("Die Zahl ist kleiner als 20")
+if (number < 25){
+    console.log(number)
+} else if (number<50) {
+    console.log("kleiner 50 " + number) 
+} else {
+    console.log("else")
 }
+*/
+
+let number = Math.random() * 100;
+let number15 = Math.round(number);
+console.log(number15)
+
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if (number15 <=20){
+    console.log("Mini")
+} 
+
+if (number15 > 22 && number15 < 50){
+    console.log("Medium")
+}
+
+if (number15 > 52){
+    console.log("Large")
+}
+
+
