b7f420f1af5593afb23021a014b8ff6316e6c17d
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2021-12-01 10:17:42 +0100

    zweiter Teil von Aufgabe 9 angefangen

--- Javascript/Aufgabe 9/XorO.js
+++ Javascript/Aufgabe 9/XorO.js
@@ -1,11 +1,34 @@
 // Aufgabe 1: Erstelle mit Hilfe einer Schleife und Modulo das folgende Ausgabe liefert (Rechteckbeispiel adaptieren)
 // % - Modulo gibt den Rest einer Division zurück
 
-console.log("Aufgabe 1 : ")
+document.write("Aufgabe 1 : " + "<br>")
 
 for (i = 0; i < 4; i++){
+    if(i % 2){
+        for(X = 0; X < 4; X++){
+            document.write("X")
+        }
+        document.write ("<br>")
+    }
+
+    else{
+        for (O = 0; O < 4; O++){
+            document.write("O");
+        }
+        document.write("<br>")
+    }
+}
+document.write("<br>")
+
+//Ausgabe ist anders herum.... aber es funktioniert
+
+//Aufgabe 2: Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren 
+//zwei Lösungswege -> Zähler dahingehend verändern, dass er nur gerade Zahlen addiert -> Modulo
+
+document.write("Aufgabe 2 : " + "<br>")
+
+for (i = 1; i < 101; i++){
     
 }
-
 
 
