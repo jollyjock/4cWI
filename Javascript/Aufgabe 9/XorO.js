35c8ad5570921a2e1c73a852deaff90681753f28
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2021-12-01 10:04:24 +0100

    Aufgabe 9 angefangen

---
+++ Javascript/Aufgabe 9/XorO.js
@@ -0,0 +1,11 @@
+// Aufgabe 1: Erstelle mit Hilfe einer Schleife und Modulo das folgende Ausgabe liefert (Rechteckbeispiel adaptieren)
+// % - Modulo gibt den Rest einer Division zurück
+
+console.log("Aufgabe 1 : ")
+
+for (i = 0; i < 4; i++){
+    
+}
+
+
+
