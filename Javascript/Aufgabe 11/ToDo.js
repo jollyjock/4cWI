7e7b352ca266b68158e03fba378f0bb8f3a4ab8d
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2021-12-15 11:08:01 +0100

    2.Video fertig

--- Javascript/Aufgabe 11/ToDo.js
+++ Javascript/Aufgabe 11/ToDo.js
@@ -1,12 +1,24 @@
-const tasks = ["putzen", "tanzen", "kochen"];
+
+//JSON javascript object notation 
+
+const tasks = [];
+console.log(tasks);
+
+
 printTaskList();
 
 
 document.getElementById("addTask").addEventListener("click",function(){
-    let newTask = document.getElementById("txtNewTask").value;
-    tasks.push(newTask);
-    printTaskList();
+    addTask();
 });
+
+function addTask(){
+    let TaskName = document.getElementById("txtNewTask").value;
+    let TaskResponsible = document.getElementById("txtResponsible").value;
+    let Task = {name: TaskName, responsible: TaskResponsible, isDone: false};
+    tasks.push(Task);
+    printTaskList();
+}
 
 
 function printTaskList(){
@@ -17,7 +29,11 @@ function printTaskList(){
 function getHTMLTasks(){
     let html = "";
     tasks.forEach(element => {
-        html += "<li>" + element + "<li>"
+        let checked = "";
+        if (element.isDone){
+            checked = "checked";
+        }
+        html += "<li><input type ='checkbox'"+ checked + "/>" +  element.name + " - " + element.responsible + "<li>"
         console.log(element)
     });
 
