5c84878031dd5a757d1edf3da2be021652cd2e0e
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2021-12-22 11:23:52 +0100

    Password Generator fertig

---
+++ Javascript/Aufgabe 12/PasswordGen.js
@@ -0,0 +1,33 @@
+var password=document.getElementById("password");
+
+
+
+document.getElementById("b1").addEventListener("click",function(){
+    genPassword();
+});
+
+document.getElementById("b2").addEventListener("click",function(){
+    copyPassword();
+});
+
+//generate Password
+ function genPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+    document.getElementById("password").value = password;
+
+}
+
+
+function copyPassword() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    document.execCommand("copy");  
+  }
+
+
