b0dbb54fab7b5a0be808d44967660ea48c02485d
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2022-01-19 10:54:24 +0100

    Minirechner fertig

---
+++ HTML & JS/Aufgabe 1/script.js
@@ -0,0 +1,39 @@
+function Buttonclicked1(){
+
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseFloat(num1) + parseFloat(num2);
+    console.log(result)
+
+}
+
+function Buttonclicked2(){
+
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseFloat(num1) - parseFloat(num2);
+    console.log(result)
+
+}
+
+function Buttonclicked3(){
+
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseFloat(num1) * parseFloat(num2);
+    console.log(result)
+
+}
+
+function Buttonclicked4(){
+
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseFloat(num1) / parseFloat(num2);
+    console.log(result)
+
+}
