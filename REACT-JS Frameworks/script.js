03bf53ef440540636539d8c79c9d92d014d952fa
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2022-04-06 19:09:21 +0200

    03 React package.json fertig

--- REACT-JS Frameworks/script.js
+++ REACT-JS Frameworks/script.js
@@ -1,1 +1,4 @@
-console.log("hello");
+var sizeOf = require('image-size');
+
+var dimensions = sizeOf ('C:\\Users\\steph\\OneDrive\\Bilder\\Dumb Shit\\Buff Steph.png');
+console.log(dimensions.width, dimensions.height);
