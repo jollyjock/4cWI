7d2cd7c059f6adc89338141bfbdfe0c30f17f773
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2022-04-06 19:39:06 +0200

    04 React first component fertig

---
+++ REACT-JS Frameworks/03 die REACT Komponente/reactdemo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SuperButoon from './componets/SuperButoon';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <SuperButoon />
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT-JS Frameworks/03 die REACT Komponente/reactdemo/src/componets/SuperButoon.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class SuperButoon extends Component{
+    render(){
+        return (<div>
+            <button>Button</button>
+        </div>)
+    }
+}
