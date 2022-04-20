89f7973e1ced44c8ee0eaa87b27f03b68b5115a8
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2022-04-20 11:26:14 +0200

    05 Aufgabe angefangen

--- REACT-JS Frameworks/03 die REACT Komponente/reactdemo/src/App.js
+++ REACT-JS Frameworks/03 die REACT Komponente/reactdemo/src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 import SuperButoon from './componets/SuperButoon';
 import ButtonRow from './componets/ButtonRow';
+import List from './componets/List';
+
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
-      <SuperButoon />
+      <SuperButoon/>
       <ButtonRow/>
       <ButtonRow/>
       <ButtonRow/>
+
+      <h2>05 Aufgabe Komponente</h2>
+      <List/>
     </div>
   );
 }
---
+++ REACT-JS Frameworks/03 die REACT Komponente/reactdemo/src/componets/List.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+
+export default class List extends List{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
