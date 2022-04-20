21a7948c8bce1cebd7abd5692bc971d8a43d4207
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2022-04-20 11:26:49 +0200

    05 immer noch dran

--- REACT-JS Frameworks/03 die REACT Komponente/reactdemo/src/componets/List.js
+++ REACT-JS Frameworks/03 die REACT Komponente/reactdemo/src/componets/List.js
@@ -4,7 +4,9 @@ import React, { Component } from "react";
 export default class List extends List{
     render(){
         return(
-            <div></div>
+            <div>
+                
+            </div>
         )
     }
 }
