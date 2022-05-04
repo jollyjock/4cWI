1adece954c03602fa6910b9db6447cdd520fde98
jollyjock <stephanie.lohitzer@student.htldornbirn.at>
2022-05-04 11:23:19 +0200

    05 Aufgabe Komponenten fertig

--- REACT-JS Frameworks/03 die REACT Komponente/reactdemo/src/componets/card.js
+++ REACT-JS Frameworks/03 die REACT Komponente/reactdemo/src/componets/card.js
@@ -5,13 +5,14 @@ import style from './card.module.css'
 
 export default class Card extends Component{
     render(){
-        return <div className={style.wrapper}>
+        return (
             <div className={style.card}>
                 <img classname={style.image} src='./card.png' alt=""></img>
-                <div className={style.heading}><h1>John Doe</h1></div>
-                <p>Architect and Engineer</p>
+                <div className={style.content}>
+                    <h1>John Doe</h1>
+                    <p>Architect and Engineer</p>
+                </div>
             </div>
-        </div>
-
+        )
     }
 }
